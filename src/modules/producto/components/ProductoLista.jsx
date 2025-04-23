0351b93a1f1a0c7c86d9d02a5066ef7e58151d10
NachoSantoro <nacho.santoro2002@gmail.com>
2025-04-23 15:21:29 -0300

    Creando catalogo de productos

---
+++ src/modules/producto/components/ProductoLista.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ProductoCard from "./ProductoCard";
+import styles from "./productoLista.module.css";
+
+const ProductoLista = ({ productos }) => {
+  return (
+    <div className={styles.grid}>
+      {productos.map((producto) => (
+        <ProductoCard key={producto.id} producto={producto} />
+      ))}
+    </div>
+  );
+};
+
+export default ProductoLista;
