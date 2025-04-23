52ec1ba441cd04358942554d087216965c288f9a
valentina fernandez <vfernandez4@uade.edu.ar>
2025-04-23 17:04:32 -0300

    fixes

--- src/modules/registro/Registro.jsx
+++ src/modules/registro/Registro.jsx
@@ -6,9 +6,13 @@ import styles from "./registro.module.css";
 const Registro = () => {
   return (
     <div className={styles["registro-container"]}>
-      <RegistroHeader />
+      <div className={styles["registro-welcome"]}>
+        <h1>¡Bienvenido!</h1>
+        <p>Por favor, crea una cuenta para comenzar.</p>
+      </div>
       <div className={styles["registro-form-container"]}>
-        <RegistroForm />
+      <RegistroHeader />
+      <RegistroForm />
       </div>
     </div>
   );
