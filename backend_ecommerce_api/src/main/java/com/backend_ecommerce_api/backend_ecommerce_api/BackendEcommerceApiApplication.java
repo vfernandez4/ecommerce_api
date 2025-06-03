d392be75476fb02d89636e4663da033bd96094bb
katerinacejas <katerinacejas3@gmail.com>
2025-06-02 21:42:29 -0300

    agrego proyecto backend

---
+++ backend_ecommerce_api/src/main/java/com/backend_ecommerce_api/backend_ecommerce_api/BackendEcommerceApiApplication.java
@@ -0,0 +1,13 @@
+package com.backend_ecommerce_api.backend_ecommerce_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEcommerceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendEcommerceApiApplication.class, args);
+	}
+
+}
