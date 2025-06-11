5e6c7d2845add4ddb001d4e7a7a8f421ccace2bd
katerinacejas <katerinacejas3@gmail.com>
2025-06-11 16:49:35 -0300

    avances backend

--- backend_ecommerce_api/src/main/java/com/backend_ecommerce_api/backend_ecommerce_api/repository/CarritoRepository.java
+++ backend_ecommerce_api/src/main/java/com/backend_ecommerce_api/backend_ecommerce_api/repository/CarritoRepository.java
@@ -1,5 +1,10 @@
 package com.backend_ecommerce_api.backend_ecommerce_api.repository;
 
-public class CarritoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.backend_ecommerce_api.backend_ecommerce_api.model.Carrito;
+
+public interface CarritoRepository extends JpaRepository<Carrito, Long> {
+	
+
 	
 }
--- backend_ecommerce_api/src/main/java/com/backend_ecommerce_api/backend_ecommerce_api/repository/UsuarioRepository.java
+++ backend_ecommerce_api/src/main/java/com/backend_ecommerce_api/backend_ecommerce_api/repository/UsuarioRepository.java
@@ -1,5 +1,18 @@
 package com.backend_ecommerce_api.backend_ecommerce_api.repository;
 
-public class UsuarioRepository {
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.backend_ecommerce_api.backend_ecommerce_api.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	
+	// Método para encontrar un usuario por su email
+	Optional<Usuario> findByEmail(String email);
+	
+	// Método para verificar si un usuario existe por su email
+	boolean existsByEmail(String email);
+	
+	// Método para eliminar un usuario por su id
+	void deleteById(Long id);
 	
 }
