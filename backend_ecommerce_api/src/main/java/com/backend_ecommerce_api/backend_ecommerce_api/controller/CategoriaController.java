dd72be2378f4492d8c1b735d3df6e2d8a1f1f7d2
Valen <164496295+vfernandez4@users.noreply.github.com>
2025-06-12 20:15:18 -0300

    Implement Categoria controller, service and repository

--- backend_ecommerce_api/src/main/java/com/backend_ecommerce_api/backend_ecommerce_api/controller/CategoriaController.java
+++ backend_ecommerce_api/src/main/java/com/backend_ecommerce_api/backend_ecommerce_api/controller/CategoriaController.java
@@ -1,5 +1,37 @@
 package com.backend_ecommerce_api.backend_ecommerce_api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.backend_ecommerce_api.backend_ecommerce_api.model.Categoria;
+import com.backend_ecommerce_api.backend_ecommerce_api.service.CategoriaService;
+
+@RestController
+@RequestMapping("/api/categorias")
 public class CategoriaController {
-	
+
+    @Autowired
+    private CategoriaService categoriaService;
+
+    @GetMapping
+    public List<Categoria> getTodasCategorias() {
+        return categoriaService.getTodasCategorias();
+    }
+
+    @PostMapping
+    public Categoria guardarCategoria(@RequestBody Categoria categoria) {
+        return categoriaService.guardarCategoria(categoria);
+    }
+
+    @GetMapping("/{id}")
+    public Categoria getCategoriaPorId(@RequestParam Long id) {
+        return categoriaService.getCategoriaPorId(id);
+    }
+
+    @PutMapping
+    public Categoria actualizarCategoria(@RequestBody Categoria categoria) {
+        return categoriaService.actualizarCategoria(categoria);
+    }
 }
