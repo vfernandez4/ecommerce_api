020395d62d07872e2f16f68dbb51553caa09e51f
Santino <santinofosco@gmail.com>
2025-06-17 18:04:01 -0300

    Agregado de funcionalidades a los endpoint por Santino, Katerina, Valentina y Juan

--- backend_ecommerce_api/src/main/java/com/backend_ecommerce_api/backend_ecommerce_api/service/ProductoService.java
+++ backend_ecommerce_api/src/main/java/com/backend_ecommerce_api/backend_ecommerce_api/service/ProductoService.java
@@ -2,6 +2,9 @@ package com.backend_ecommerce_api.backend_ecommerce_api.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.backend_ecommerce_api.backend_ecommerce_api.repository.ProductoRepository;
 import com.backend_ecommerce_api.backend_ecommerce_api.model.Producto;
@@ -37,5 +40,29 @@ public class ProductoService {
             return this.productoRepository.save(producto);
         }
         return null;
+    }
+
+    public void eliminarProducto(Long id) {
+        if (this.productoRepository.existsById(id)) {
+            this.productoRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("Producto no encontrado con el id:" + id);
+        }
+    }
+
+    public List<Producto> getProductosPorCategoria(String categoria) {
+        return this.productoRepository.findByCategoria_Nombre(categoria);
+    }
+
+    public List<Producto> getProductosPorNombre(String nombre) {
+        return this.productoRepository.findByNombreContainingIgnoreCase(nombre);
+    }
+
+    public List<Producto> getProductosPublicados(String email) {
+        return this.productoRepository.findByVendedor_Email(email);
+    }
+
+    public List<Producto> getProductosDestacados() {
+        return this.productoRepository.findDestacados();
     }
 }
